fffd2185244a10b85953b6d8c38f03e9bc2ff117
syproduction <syproduction@mail.ru>
2021-10-19 19:24:35 +0500

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -103,14 +103,13 @@ sudo apt-get install cmus -y
 sudo cp ~/ipodrpi/system/alsa-base.conf /etc/modprobe.d/alsa-base.conf
 sudo rm /etc/pikeyd.conf
 sudo cp ~/ipodrpi/etc/pikeyd.conf.cmus /etc/pikeyd.conf
-#cmus :bind -f common u shell ~/.config/cmus/cmus-update.sh &
 cmus &
+cmus-remote -C "bind -f common u shell ~/.config/cmus/cmus-update.sh" &
+cmus-remote -C "set softvol=true" &
 sudo mkdir ~/Music
 sudo cp ~/ipodrpi/system/rc ~/.config/cmus/rc
 sudo cp ~/ipodrpi/system/cmus-update.sh ~/.config/cmus/cmus-update.sh
 sudo chmod +x ~/.config/cmus/cmus-update.sh
-cmus-remote -C "bind -f common u shell ~/.config/cmus/cmus-update.sh"&
-cmus-remote -C "set softvol=true" &
 echo "cmus" >> ~/.bashrc
 #echo "~/.config/cmus/cmus-update.sh" >> ~/.bashrc
 echo -e "\e[1;36m IF NO SOUND IN CMUS, SET IT UP IN RASPI-CONFIG \e[0m"
